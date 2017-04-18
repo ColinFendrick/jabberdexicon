362901fff7f412b421743aced057d7c333c0bfb7
Colin Fendrick <colin.fendrick@gmail.com>
2017-04-18 12:54:41 -0400

    Can update form fields and whatnot

--- src/components/AddWord.js
+++ src/components/AddWord.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react'
 import styles from '../styles/AddWord.scss'
 
 class AddWord extends Component {
+  _submit = e => {
+    e.preventDefault()
+    console.log(this.refs.newTerm.value, this.refs.newDef.value)
+  }
+
   render () {
-    return <div className={styles.addWord}>
-      <input type='text' defaultValue='New Term' className={styles.newTerm} />
-      <input type='text' defaultValue='New Definition' className={styles.newDefinition} />
-      <input type='button' value='Add a new word' />
-    </div>
+    return <form onSubmit={this._submit} className={styles.addWord}>
+      <input type='text' ref='newTerm' defaultValue='New Term' className={styles.newTerm} />
+      <input type='text' ref='newDef' defaultValue='New Definition' className={styles.newDefinition} />
+      <input type='submit' value='Add a new word' />
+    </form>
   }
 }
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component {
+  _submit = e => {
+    e.preventDefault()
+    console.log(this.refs.searchText.value)
+  }
+
   render () {
     return <div className='SearchBar'>
-      <input type='text' /><input type='button' value='Search' />
+      <form onSubmit={this._submit}>
+        <input type='text' ref='searchText' />
+        <input type='submit' value='Search' />
+      </form>
     </div>
   }
 }
